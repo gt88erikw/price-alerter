31a7e379eedfeceaee5c2cc0e26a5ac21831db3d
gt88erikw <44692311+gt88erikw@users.noreply.github.com>
2019-07-04 15:51:08 -0500

    set URI in code

--- common/database.py
+++ common/database.py
@@ -5,9 +5,9 @@ from typing import Dict
 
 
 class Database:
-    # URI = "mongodb://127.0.0.1:27017/pricing"
-    URI = os.environ.get('MONGODB_URI')
-    DATABASE = pymongo.MongoClient(URI).get_default_database()
+    URI = "mongodb://127.0.0.1:27017/pricing"
+    # URI = os.environ.get('MONGODB_URI')
+    DATABASE = pymongo.MongoClient(URI).get_database()
 
     @staticmethod
     def insert(collection: str, data: Dict):
